ce30c13043ee0c9e9ac860500dfb8355da3b74bc
Md. Rabiul Hasan <rabiul.fci@gmail.com>
2021-11-30 02:15:23 +0600

    create a container component

---
+++ src/components/common/Container/styles.js
@@ -0,0 +1,7 @@
+import {StyleSheet} from 'react-native';
+const styles = StyleSheet.create({
+    wrapper:{
+        padding:100
+    }
+});
+export default styles;
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Container from '../../components/common/container';
 
 const Login = () => {
     return (
         <View>
-            <Text>Login Page</Text>
+            <Container style={{ padding:20 }}>
+                <Text>Login Page</Text>
+            </Container>
+            
         </View>
     );
 };
